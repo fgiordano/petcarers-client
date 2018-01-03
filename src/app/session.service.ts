31198ebb5fc387c419efbb050ad7e9dd57643d0a
Francesca Giordano <francesca020406@hotmail.com>
2018-01-02 23:05:50 -0500

    adds env

--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -1,46 +1,147 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+import { environment } from '../../environments/environment';
 
 @Injectable()
-export class SessionService {
+export class AuthServiceService {
 
-  constructor(private http: Http) { }
+  constructor(
+    private httpThang: Http
+  ) { }
 
-  handleError(e) {
-  	return Observable.throw(e.json().message);
-  }
 
-  signup(user) {
-  	return this.http.post(`/signup`, user)
-  	.map(res => res.json())
-  	.catch(this.handleError);
-  }
+  // an argument for each "req.body" in the API route
+  signup(componentInfo) {
+      return this.httpThang
+        .post(
+          `${environment.apiBase}/api/signup`,
 
-  login(user) {
-  	return this.http.post(`login`, user)
-  	.map(res => res.json())
-  	.catch(this.handleError);
-  }
+          // Form body information to send to the back end (req.body)
+          {
+            signupFullName: componentInfo.fullName,
+            signupEmail: componentInfo.email,
+            signupPassword: componentInfo.password
+          },
+
+          // Send the cookies across domains
+          { withCredentials: true }
+        )
+
+        // Convert from observable to promise
+        .toPromise()
+
+        // Parse the JSON
+        .then(res => res.json());
+  } // close signup()
+
+
+  login(componentInfo) {
+      return this.httpThang
+        .post(
+          `${environment.apiBase}/api/login`,
+
+          // Form body information to send to the back end (req.body)
+          {
+            blahEmail: componentInfo.email,
+            blahPassword: componentInfo.password
+          },
+
+          // Send the cookies across domains
+          { withCredentials: true }
+        )
+
+        // Convert from observable to promise
+        .toPromise()
+
+        // Parse the JSON
+        .then(res => res.json());
+  } // close login()
+
 
   logout() {
-  	return this.http.post(`logout`, {})
-  	.map(res => res.json())
-  	.catch(this.handleError);
-  }
+      return this.httpThang
+        .post(
+          `${environment.apiBase}/api/logout`,
 
-  isLoggedIn() {
-  	return this.http.get(`/loggedin`)
-  	.map(res => res.json())
-  	.catch(this.handleError);
-  }
+          // Nothing to send to the back end (req.body)
+          {},
 
-  getPrivateData() {
-  	return this.http.get(`/private`)
-  	.map(res => res.json())
-  	.catch(this.handleError);
-  }
+          // Send the cookies across domains
+          { withCredentials: true }
+        )
+
+        // Convert from observable to promise
+        .toPromise()
+
+        // Parse the JSON
+        .then(res => res.json());
+  } // close logout()
+
+
+  checklogin() {
+      return this.httpThang
+        .get(
+          `${environment.apiBase}/api/checklogin`,
+
+          // Send the cookies across domains
+          { withCredentials: true }
+        )
+
+        // Convert from observable to promise
+        .toPromise()
+
+        // Parse the JSON
+        .then(res => res.json());
+  } // close checklogin()
 
 }
+
+
+// import { Injectable } from '@angular/core';
+// import { Http, Response } from '@angular/http';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/catch';
+// import { Observable } from 'rxjs/Observable';
+
+// @Injectable()
+// export class SessionService {
+
+//   constructor(private http: Http) { }
+
+//   handleError(e) {
+//   	return Observable.throw(e.json().message);
+//   }
+
+//   signup(user) {
+//   	return this.http.post(`/signup`, user)
+//   	.map(res => res.json())
+//   	.catch(this.handleError);
+//   }
+
+//   login(user) {
+//   	return this.http.post(`login`, user)
+//   	.map(res => res.json())
+//   	.catch(this.handleError);
+//   }
+
+//   logout() {
+//   	return this.http.post(`logout`, {})
+//   	.map(res => res.json())
+//   	.catch(this.handleError);
+//   }
+
+//   isLoggedIn() {
+//   	return this.http.get(`/loggedin`)
+//   	.map(res => res.json())
+//   	.catch(this.handleError);
+//   }
+
+//   getPrivateData() {
+//   	return this.http.get(`/private`)
+//   	.map(res => res.json())
+//   	.catch(this.handleError);
+//   }
+
+// }
