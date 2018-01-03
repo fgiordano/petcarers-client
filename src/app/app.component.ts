4c5800330fc217cdfd517498c7bd1a9f64218c67
Francesca Giordano <francesca020406@hotmail.com>
2018-01-02 23:11:06 -0500

    removes session service import from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SessionService } from './session.service';
+// import { SessionService } from './session.service';
 
 @Component({
   selector: 'app-root',
