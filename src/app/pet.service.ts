8b34f8c9ddff38d093afcc116b3b5fabaa6d8ca2
Francesca Giordano <francesca020406@hotmail.com>
2018-01-08 19:15:41 -0500

    adds pet service logic

--- src/app/pet.service.ts
+++ src/app/pet.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class PetService {
 
-  constructor() { }
+  constructor(
+
+    private httpThang: Http
+  ) { }
+
+
+  newPet(componentInfo) {
+      return this.httpThang
+        .post(
+          `${environment.apiBase}/api/pets`,
+
+          // Form body information to send to the back end (req.body)
+          componentInfo,
+
+          // Send the cookies across domains
+          { withCredentials: true }
+        )
+
+        // Parse the JSON
+        .map(res => res.json());
+  } // close newCamel()
+
+
+  allPets() {
+      return this.httpThang
+        .get(
+          `${environment.apiBase}/api/pets`,
+
+          // Send the cookies across domains
+          { withCredentials: true }
+        )
+
+        // Parse the JSON
+        .map(res => res.json());
+  } // close allCamels()
 
 }
